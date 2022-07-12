59b50c2de9790f06bdba4e89c181abfc1c517a9a
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-07-12 18:32:03 +0200

    new change

--- twitter/components/SideBarRow.tsx
+++ twitter/components/SideBarRow.tsx
@@ -9,7 +9,7 @@ function SideBarRow({Icon,title}:Props) {
   return (
     <div className="group flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full transition-all duration-200 hover:bg-gray-200">
         <Icon className='h-6 w-6'/>
-        <p className="hidden md:inline-flex group-hover:text-twitter">{title}</p>
+        <p className="hidden md:inline-flex group-hover:text-twitter text-base font-light lg:text-xl">{title}</p>
     </div>
   )
 }
