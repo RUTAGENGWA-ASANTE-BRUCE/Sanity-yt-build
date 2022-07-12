eaa4fc99cd47ff7529278d235a32b6b50a64fe5d
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-07-12 14:30:34 +0200

    new change

--- twitter/components/SideBar.tsx
+++ twitter/components/SideBar.tsx
@@ -13,7 +13,7 @@ import SideBarRow from './SideBarRow'
 
 function SideBar() {
   return (
-    <div className="">
+    <div className="flex flex-col col-span-2 items-center px-4 md:items-start">
         <img src="https://links.papareact.com/drq" className="h-10 w-10" alt="" />
         <SideBarRow Icon={HomeIcon}  title={"Home"} />
         <SideBarRow Icon={HashtagIcon}  title={"Explore"} />
@@ -21,6 +21,9 @@ function SideBar() {
         <SideBarRow Icon={MailIcon}  title={"Messages"} />
         <SideBarRow Icon={BookmarkIcon}  title={"Bookmarks"} />
         <SideBarRow Icon={CollectionIcon}  title={"Lists"} />
+        <SideBarRow Icon={UserIcon}  title={"Signin"} />
+        <SideBarRow Icon={DotsCircleHorizontalIcon}  title={"More"} />
+
     </div>
   )
 }
